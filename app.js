07913345e73abc3fe716fb843e5714d83dda9f41
Helder Marques Roque <82022902+Hroque1987@users.noreply.github.com>
2021-07-03 18:13:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 
 const homeStartingContent = "Welcome to my blog";
 const aboutContent = "This is a blog i created to practice on Web Developement. I used Node.js, with the extensions Express, Body Parser and EJS. ";
-const contactContent = "You can contact me at h.roque1987@gmail.com";
+const contactContent = "https://github.com/Hroque1987";
 
 const app = express();
 
@@ -18,78 +18,52 @@ app.use(express.static("public"));
 
 let posts = [];
 
-app.get("/", function(req, res){
-  
+app.get("/", function(req, res){  
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
-  });
-
-  console.log(posts);
-  
-  
+  }); 
 });
 
+
 app.get("/posts/:postName", function(req,res){
-
   const requestedName = _.lowerCase(req.params.postName);
-
   posts.forEach(function(post){
-
     const postTitle = _.lowerCase(post.title);
-
     if ( postTitle === requestedName ) {
       const title = posts
       res.render ("post" , {
         title:post.title ,
         content: post.content
       });
-    };
-      
-  
-  });
-
-  
-
+    };   
+   });
 });
 
 app.get("/about", function(req, res) {
-
   res.render("about", {aboutContent: aboutContent});
-
 });
 
 
 app.get("/contact", function(req, res) {
-
   res.render("contact", {contactContent: contactContent});
-
 });
 
 
 app.get("/compose", function(req, res) {
-
   res.render("compose");
-
 });
 
 
 
 app.post("/compose", function(req, res) {
-
   const blogPost = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
-
   posts.push(blogPost);
-  res.redirect("/");
-  
-    
+  res.redirect("/");    
 });
-
-
-
 
 
 app.listen(3000, function() {
